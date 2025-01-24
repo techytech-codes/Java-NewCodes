f1036831b27b20b23d940e27365a8f16066269eb
Tech-Techycodes <techytech.code@gmail.com>
2025-01-24 12:37:21 +0530

    new upload

--- NewJava.java
+++ NewJava.java
@@ -2,7 +2,12 @@
 public class NewJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(int r=5;r>=1;r--) {
+			for(int c=5;c>=r;c--) {
+				System.out.print(c+" ");
+			}
+			
+		}
 
 	}
 
